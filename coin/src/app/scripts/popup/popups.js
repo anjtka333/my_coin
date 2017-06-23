57864470f899ab8d140426f95031e6ba3a765b59
Anna <andreikiv.ann@gmail.com>
2017-06-23 14:35:54 +0300

    everising working

--- coin/src/app/scripts/popup/popups.js
+++ coin/src/app/scripts/popup/popups.js
@@ -33,7 +33,8 @@ var angularModule = angular.module('popUps', []);
             , img: pop.img_icon
         });
         pop.spendName = '';
-        pop.img = "img/bar.png";
+        pop.img = "../../img/popup-img/bar.png";
+        pop.submitted = false;
     };
     pop.addCircle = function () {
         $('#myModal').modal('hide');
@@ -114,3 +115,137 @@ var angularModule = angular.module('popUps', []);
         pop.submitted = false;
     }
 });
+
+
+// angularModule.directive('scApp', function (){
+//   return {
+//       restrict: 'E',
+//       replace: true,
+//       scope: {},
+//       templateUrl: './spendsPopup.html',
+//
+//         // controller: ['pop', function ($http) {
+//         //     var pop = this;
+//         //
+//         //     $http.get("https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11").then(function (response) {
+//         //         pop.USDtoUAH = response.data[0].buy;
+//         //         pop.EURtoUAH = response.data[1].buy;
+//         //         return (pop.EURtoUAH);
+//         //
+//         //     });
+//         //     pop.circles = [
+//         //         {
+//         //             savingsName: 'bla'
+//         //             , savingsAmmount: 555
+//         //             , currency: "UAH"
+//         //             , img: "../../img/popup-img/bar.png"
+//         //         }
+//         //     ];
+//         //     pop.balance = 0;
+//         //     pop.circlesSpend = [
+//         //         {
+//         //             spendName: 'girl'
+//         //             , img: "../../img/popup-img/bar.png"
+//         //         }
+//         //
+//         //
+//         //     ];
+//         //     pop.addSpendCircle = function () {
+//         //         $('#myModal3').modal('hide');
+//         //         pop.circlesSpend.push({
+//         //             spendName: pop.spendName
+//         //             , img: pop.img_icon
+//         //         });
+//         //         pop.spendName = '';
+//         //         pop.img = "img/bar.png";
+//         //     };
+//         //     pop.addCircle = function () {
+//         //         $('#myModal').modal('hide');
+//         //         console.log(pop.EURtoUAH);
+//         //         if (pop.savingsAmmount == undefined) {
+//         //             pop.savingsAmmount = 0;
+//         //         }
+//         //         if (pop.checked) {
+//         //             console.log(pop.data.selectedOption.name);
+//         //             if (pop.data.selectedOption.name == 'USD') {
+//         //                 pop.balance += pop.savingsAmmount * pop.USDtoUAH;
+//         //             }
+//         //             else if (pop.data.selectedOption.name == 'UAH') {
+//         //                 pop.balance += pop.savingsAmmount;
+//         //             }
+//         //             else {
+//         //                 pop.balance += pop.savingsAmmount * pop.EURtoUAH;
+//         //             }
+//         //         }
+//         //         pop.circles.push({
+//         //             savingsName: pop.savingsName
+//         //             , savingsAmmount: pop.savingsAmmount
+//         //             , currency: pop.data.selectedOption.name
+//         //             , img: pop.img_icon
+//         //         });
+//         //         pop.savingsName = '';
+//         //         pop.savingsAmmount = 0;
+//         //         pop.currency = "UAH";
+//         //         pop.submitted = false;
+//         //     };
+//         //     pop.data = {
+//         //         availableOptions: [
+//         //             {
+//         //                 id: '1',
+//         //                 name: 'USD'
+//         //             }
+//         //             , {
+//         //                 id: '2'
+//         //                 , name: 'EUR'
+//         //             }
+//         //             , {
+//         //                 id: '3'
+//         //                 , name: 'UAH'
+//         //             }
+//         //         ]
+//         //         , selectedOption: {
+//         //             id: '3'
+//         //             , name: 'UAH'
+//         //         } //This sets the default value of the select in the ui
+//         //     };
+//         //     pop.resetDropDown = function () {
+//         //         pop.data = {
+//         //             availableOptions: [
+//         //                 {
+//         //                     id: '1'
+//         //                     , name: 'USD'
+//         //                 }
+//         //                 , {
+//         //                     id: '2'
+//         //                     , name: 'EUR'
+//         //                 }
+//         //                 , {
+//         //                     id: '3'
+//         //                     , name: 'UAH'
+//         //                 }
+//         //             ]
+//         //             , selectedOption: {
+//         //                 id: '3'
+//         //                 , name: 'UAH'
+//         //             } //This sets the default value of the select in the ui
+//         //         };
+//         //         pop.img_icon = "../../img/popup-img/bar.png";
+//         //         pop.submitted = false;
+//         //         pop.checked = false;
+//         //     }
+//         //     pop.setImg = function () {
+//         //         pop.img_icon = "../../img/popup-img/bar.png";
+//         //         pop.submitted = false;
+//         //     }
+//         // }]
+//   };
+// }
+//
+// );
+
+// angular.module('popUps').directive('userInfoCard',function () {
+//     return {
+//         template: "Name: {{pop.circles.savingsName}}"
+//     }
+//
+// })
